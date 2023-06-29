5b7cf0497bec21644a8bd3f6c19d80381dd635d9
Kseniya Yaskevich <yaskevi4kseniya@gmail.com>
2023-06-29 09:33:47 +0300

    выполнен урок 16

--- script.js
+++ script.js
@@ -1,57 +1,15 @@
-const num = 266219;
-let total = 1;
-
-const arrayOfNumbers = num.toString().split('').map(Number);
-
-arrayOfNumbers.forEach((elem) => {
-  total *= elem;
-});
-
-console.log(`Произведение цифр числа ${num} -`, total);
-console.log(`Полученный результат возвести в степень 3 -`, total ** 3);
-console.log(`Вывести в консоль первые 2 цифры полученного числа -`, (total ** 3).toString().slice(0, 2));
-
-// УРОК 3
-
-//1
-let lang = 'en';
-let weekDays = {
-  ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-  en: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-};
-let result;
-
-//1a) 
-if (lang === 'ru') {
-  result = weekDays.ru;
-} else {
-  result = weekDays.en;
+class First {
+  hello() {
+    console.log('Привет я метод родителя!');
+  }
 }
 
-console.log('a) через if:', result.join(', '));
-
-//1b) 
-switch (lang) {
-  case 'ru':
-    result = weekDays.ru;
-    break;
-  case 'en':
-    result = weekDays.en;
-    break;
-  default:
-    result = weekDays.ru;
-    break;
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log('А я наследуемый метод!!');
+  }
 }
 
-console.log('b) через switch-case:', result.join(', '));
-
-//1c)
-result = weekDays[lang];
-
-console.log('c) через многомерный массив:', result.join(', '));
-
-//2
-let namePerson = 'Кто-то другой';
-
-let output = namePerson === 'Артем' ? console.log('Директор') :
-  namePerson === 'Александр' ? console.log('Преподаватель') : console.log('Студент');
+const second = new Second();
+second.hello();
